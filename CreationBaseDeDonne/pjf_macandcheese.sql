ac00a78d8f05218b762c0ef4729c2a229f7b87dd
Amine <amine.benslimane@gmail.com>
2021-04-15 17:38:34 -0400

    DB

---
+++ CreationBaseDeDonne/pjf_macandcheese.sql
@@ -0,0 +1,78 @@
+--
+-- Base de données : `pjf_macandcheese`
+--
+
+DROP DATABASE IF EXISTS pjf_macandcheese;
+CREATE DATABASE IF NOT EXISTS pjf_macandcheese;
+USE pjf_macandcheese;
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `NoUtilisateur` int NOT NULL AUTO_INCREMENT,
+  `Courriel` varchar(50) DEFAULT NULL,
+  `MotDePasse` varchar(15) DEFAULT NULL,
+  `Creation` datetime DEFAULT NULL,
+  `NbConnexions` int DEFAULT NULL,
+  `Statut` int DEFAULT NULL,
+  `NoEmpl` int DEFAULT NULL,
+  `Nom` varchar(25) DEFAULT NULL,
+  `Prenom` varchar(20) DEFAULT NULL,
+  `NoTelMaison` varchar(15) DEFAULT NULL,
+  `NoTelTravail` varchar(21) DEFAULT NULL,
+  `NoTelCellulaire` varchar(15) DEFAULT NULL,
+  `Modification` datetime DEFAULT NULL,
+  `AutresInfos` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`NoUtilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `connexions`
+--
+
+DROP TABLE IF EXISTS `connexions`;
+CREATE TABLE IF NOT EXISTS `connexions` (
+  `NoConnexion` int NOT NULL AUTO_INCREMENT,
+  `NoUtilisateur` int NOT NULL,
+  `Connexion` datetime DEFAULT NULL,
+  `Deconnexion` datetime DEFAULT NULL,
+  PRIMARY KEY (`NoConnexion`),
+  CONSTRAINT `FK_NoUtilisateur_Connexions` FOREIGN KEY (`NoUtilisateur`) REFERENCES `utilisateurs` (`NoUtilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `NoCategorie` int NOT NULL AUTO_INCREMENT,
+  `Description` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`NoCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Structure de la table `annonces`
+--
+
+DROP TABLE IF EXISTS `annonces`;
+CREATE TABLE IF NOT EXISTS `annonces` (
+  `NoAnnonce` int NOT NULL AUTO_INCREMENT,
+  `NoUtilisateur` int NOT NULL,
+  `Parution` datetime DEFAULT NULL,
+  `Categorie` int NOT NULL,
+  `DescriptionAbregee` varchar(50) DEFAULT NULL,
+  `DescriptionComplete` varchar(250) DEFAULT NULL,
+  `Prix` numeric(15,2) DEFAULT NULL,
+  `Photo` varchar(50) DEFAULT NULL,
+  `MiseAJour` datetime DEFAULT NULL,
+  `Etat` int DEFAULT NULL,
+  PRIMARY KEY (`NoAnnonce`),
+  CONSTRAINT `FK_NoUtilisateur_Annonces` FOREIGN KEY (`NoUtilisateur`) REFERENCES `utilisateurs` (`NoUtilisateur`),
+  CONSTRAINT `Categorie` FOREIGN KEY (`Categorie`) REFERENCES `categories` (`NoCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
---
+++ CreationBaseDeDonne/scriptBaseDeDonne.php
@@ -0,0 +1,60 @@
+<?php
+    require_once("../dbconfig.php");
+    require_once("classe-fichier-2021-03-07.php");
+
+    // Creation de la db
+    $cBD = new PDO("mysql:host=localhost", $strNomAdmin, $strMotPasseAdmin);
+    $requete = file_get_contents("pjf_macandcheese.sql");
+    $preparerExec = $cBD->prepare($requete);
+    $resultat = $preparerExec->execute();
+    if ($resultat){
+        echo "La base de donnée pjf_macandcheese a été créée";
+    }else{ 
+        echo "La base de donnée pjf_macandcheese n'a pas pu être créée";
+        exit();
+    }
+
+    $fichierUtilisateur = new fichier("utilisateurs.csv");
+    $strNomDeTable = "utilisateurs";
+    $fichierUtilisateur->ouvre();
+    while (!$fichierUtilisateur->detecteFin()){
+       $tValeur = explode(";", $fichierUtilisateur->litLigne());
+       $requete = "INSERT INTO $strNomDeTable VALUES (default,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+       $preparerExec= $cBD->prepare($requete);
+       $preparerExec->execute([$tValeur[1],$tValeur[2],$tValeur[3],$tValeur[4],$tValeur[5],$tValeur[6],$tValeur[7],$tValeur[8],$tValeur[9],$tValeur[10],$tValeur[11],$tValeur[12],$tValeur[13]]);
+    }
+    $fichierUtilisateur->ferme();
+
+    $fichierConnexions = new fichier("connexions.csv");
+    $strNomDeTable = "connexions";
+    $fichierConnexions->ouvre();
+    while (!$fichierConnexions->detecteFin()){
+       $tValeur = explode(";", $fichierConnexions->litLigne());
+       $requete = "INSERT INTO $strNomDeTable VALUES (default,?,?,?)";
+       $preparerExec= $cBD->prepare($requete);
+       $preparerExec->execute([$tValeur[1],$tValeur[2],$tValeur[3]]);
+    }
+    $fichierConnexions->ferme();
+
+    $fichierCategories = new fichier("categories.csv");
+    $strNomDeTable = "categories";
+    $fichierCategories->ouvre();
+    while (!$fichierCategories->detecteFin()){
+       $tValeur = explode(";", $fichierCategories->litLigne());
+       $requete = "INSERT INTO $strNomDeTable VALUES (default,?)";
+       $preparerExec= $cBD->prepare($requete);
+       $preparerExec->execute([$tValeur[1]]);
+    }
+    $fichierCategories->ferme();
+
+    $fichierAnnonces = new fichier("annonces.csv");
+    $strNomDeTable = "annonces";
+    $fichierAnnonces->ouvre();
+    while (!$fichierAnnonces->detecteFin()){
+       $tValeur = explode(";", $fichierAnnonces->litLigne());
+       $requete = "INSERT INTO $strNomDeTable VALUES (default,?,?,?,?,?,?,?,?,?)";
+       $preparerExec= $cBD->prepare($requete);
+       $preparerExec->execute([$tValeur[1],$tValeur[2],$tValeur[3],$tValeur[4],$tValeur[5],$tValeur[6],$tValeur[7],$tValeur[8],$tValeur[9]]);
+    }
+    $fichierAnnonces->ferme();
+?>
