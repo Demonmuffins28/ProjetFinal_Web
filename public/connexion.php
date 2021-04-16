0192bd6c74b30e549865e11e1f3a351128f87595
Chuck <charles.simon.morin@gmail.com>
2021-04-16 03:01:16 -0400

    Création de la page Connexion.php et Index.php

--- public/connexion.php
+++ public/connexion.php
@@ -13,63 +13,75 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Les Petites Annonces GG</title>
-  <!-- CSS only -->
-  <link rel="stylesheet" type="text/css" href="index.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <script defer src="../node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
-  <script defer src="../node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
-  <script defer src="../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Les Petites Annonces GG</title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script defer src="../node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
+        <script defer src="../node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
+        <script defer src="../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
+    </head>
+    <style>
+        html,body {
+            height: 100%;
+            background-color: rgb(77, 77, 77);
+        }
+        .form-group {
+            font-size: large;
+            width: calc(100% - 25rem);
+            font-size: 30px;
+        }
 
+    </style>
 
-<body class="container" class="row">
-
-    <img src="" ></img>
-
-    <form method="get" action="" class="col">
-
-        <!-- Bouton s'inscrire -->
-        <!-- Logo du service d'annonce -->
-        <img src="../images/logo.jpeg" class="col-sm-6"></img>
-        <!-- Boite de Connexion -->
-        <div class="border rounded col-sm-6">
-
-            <!-- Titre de la boite -->
-            <div id="divonnexion" class="form-row text-center justify-content-center">
-                <div class="col-sm-4">
-                    <h1 id="connexion">Ouvrir une session</h1>
+    <body>
+        <div class="container-fluid h-100">
+            <div class="row h-100">
+            <!-- Logo du service d'annonce -->
+                <div class="col-5 border-3 border-end border-color h-100 bg-dark border-secondary">
+                <img src="../images/logobd2.png" class="img-fluid h-100">
                 </div>
-            </div>
+                <div class="col-7 h-100" method="POST" action="connexion.php">
 
-            <!-- Adresse Courriel -->
-            <div class="form-group has-feedback w-25">
-                <label class="control-label">Adresse de courriel</label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="Adresse de courriel"/>
-            </div>
+                    <!-- Bouton s'inscrire -->
+                    <div class="d-flex justify-content-end p-5">
+                        <a class="btn btn-primary" style="width: 20%;" href="#" role="button"><h4>S'inscrire!</h4></a>
+                    </div>
 
-            <!-- Mot de Passe -->
-            <div class="form-row form-group justify-content-center">
-                <div class="col-sm-4">
-                    <label for="password">Mot de passe</label>
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Mot de Passe" />
-                </div>
-            </div>
+                    <form class="d-flex flex-column justify-content-start align-items-center h-75 w-100">
 
-            <!-- Boutton Connexion-->
-            <div class="form-row form-group justify-content-center">
-                <div class="col-sm-4">
-                    <input type="submit" id="btn" class="btn btn-primary btn-block" value="Connexion" />
+
+                        <div class="form-group col-6 p-3">
+                            <label><h1>Ouvrir une session</h1></label>
+                        </div>
+
+                        <!-- Adresse de courriel -->
+                        <div class="form-group col-6 p-3">
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Adresse courriel" style="font-size : 20px;"/>
+                        </div>
+
+                        <!-- Mot de passe -->
+                        <div class="form-group col-6 p-3">
+                            <input type="password" id="password" name="password" class="form-control" placeholder="Mot de Passe" style="font-size : 20px;"/>
+                        </div>
+
+                        <!-- Bouton connexion -->
+                        <div class="form-group col-6 p-3">
+                            <input type="submit" id="btnConnexion" class="btn btn-primary btn-block w-100 font" value="Connexion" style="font-size : 25px;" />
+                        </div>
+
+                        <!-- Mot de passe oublié? -->
+                        <a href="#" class="link-secondary p-5"><h5>Mot de passe oublié?<h5></a>
+
+                    </form>
+
                 </div>
             </div>
         </div>
-    </form>
-
-</body>
+    </body>
 
 </html>
