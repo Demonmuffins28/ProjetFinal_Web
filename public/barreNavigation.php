5d30d29a3221f664336df28871ead038ad8c8218
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2021-04-21 13:56:23 -0400

    Les validaitons presques 100% terminer

--- public/barreNavigation.php
+++ public/barreNavigation.php
@@ -34,9 +34,11 @@ $strCouleur = "#e66465";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="index.css">
+
   <script defer src="../node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
   <script defer src="../node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
   <script defer src="../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
 <body>
--- public/libValidation.php
+++ public/libValidation.php
@@ -1,49 +1,49 @@
 <?php
-    /**
-     * Validation en php
-     */
- 
+
+/**
+ * Validation en php
+ */
+
 ?>
 
 <script>
 /**
-  * Validation en JS
-  */
+ * Validation en JS
+ */
 
-  function validationEmail(strEmail){
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(strEmail);
-  }
+function validationEmail(strEmail) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(strEmail);
+}
 
-  function validationMotDePasse(strMotDePasse){
-    return strMotDePasse.length >= 5 && strMotDePasse.length <= 15
-  }
+function validationMotDePasse(strMotDePasse) {
+  return strMotDePasse.length >= 5 && strMotDePasse.length <= 15
+}
 
-  function validationNomPrenom(strNomPrenom){
-    return /^[A-Za-z\u00C0-\u017F]+([A-Za-z\u00C0-\u017F ]|'|-){1}[A-Za-z\u00C0-\u017F]+$/.test(strNomPrenom);
-  }
+function validationNomPrenom(strNomPrenom) {
+  return /^[A-Za-z\u00C0-\u017F]+([A-Za-z\u00C0-\u017F ]|'|-){1}[A-Za-z\u00C0-\u017F]+$/.test(strNomPrenom);
+}
 
-  function validationTelMaisonCellulaire(strTel){
-    return /^\(\d{3}\)\s\d{3}-\d{4}(P|N)$/.test(strTel);;
-  }
+function validationTelMaisonCellulaire(strTel) {
+  return /^\(\d{3}\)\s\d{3}-\d{4}$/.test(strTel);;
+}
 
-  function validationTelTravail(strTel){
-    return /^\(\d{3}\)\s\d{3}-\d{4}\s#\d{4}(P|N)$/.test(strTel);;
-  }
+function validationTelTravail(strTel) {
+  return /^\(\d{3}\)\s\d{3}-\d{4}\s#\d{4}$/.test(strTel);;
+}
 
-  function validationStatutUtilisateurs(strStatut){
-    return /^[0-5]{1}$/.test(strStatut); 
-  }
+function validationStatutUtilisateurs(strStatut) {
+  return /^[0-5]{1}$/.test(strStatut);
+}
 
-  function validationEtatAnnonce(strEtat){
-    return /^[1-3]{1}$/.test(strEtat); 
-  }
+function validationEtatAnnonce(strEtat) {
+  return /^[1-3]{1}$/.test(strEtat);
+}
 
-  function validationNoEmployer(strNoEmployer){
-    return /^[1-9]{1}[0-9]{0,3}$/.test(strNoEmployer); 
-  }
+function validationNoEmployer(strNoEmployer) {
+  return /^[1-9]{1}[0-9]{0,3}$/.test(strNoEmployer);
+}
 
-  function validationPrix(strPrix){
-    return isNaN(parseFloat(strPrix)) ? false : parseFloat(strPrix) >= 0 &&  parseFloat(strPrix) <= 99999.99
-  }
-
+function validationPrix(strPrix) {
+  return isNaN(parseFloat(strPrix)) ? false : parseFloat(strPrix) >= 0 && parseFloat(strPrix) <= 99999.99
+}
 </script>
