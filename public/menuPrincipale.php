058b91f76e3255e637526ff9f1e506f2328d3bc9
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2021-04-28 10:27:56 -0400

    afficher annonces

--- public/menuPrincipale.php
+++ public/menuPrincipale.php
@@ -5,6 +5,110 @@ require_once("barreNavigation.php");
 
 ?>
 
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/airpods.PNG" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">AirPods usé beaucoup trop</h5>
+            <p class="card-text">C$60
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img1.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Air Jordan noir</h5>
+            <p class="card-text">C$169
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">PlayStation 4 avec manette invisible</h5>
+            <p class="card-text">C$460
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Beats Air 2</h5>
+            <p class="card-text">C$122
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img4.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Samsung Galaxy S10 128GB Prism Blue</h5>
+            <p class="card-text">C$450
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img5.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Scooter 330HP</h5>
+            <p class="card-text">C$6050
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm">
+      <div class=annonce>
+        <div class="card" style="width: 18rem;">
+          <img src="../images/img6.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Voiturette tel que vue
+            </h5>
+            <p class="card-text">C$5
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </body>
 
 </html>
