f6cb3c5b7bb9b20220d4fe2e22bd2afe46a805e4
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2021-05-11 23:04:33 -0400

    gitflow-feature-stash: annonces

--- public/menuPrincipale.php
+++ public/menuPrincipale.php
@@ -3,111 +3,76 @@ $binAffichageAnnonce = true;
 
 require_once("barreNavigation.php");
 
+// Variable pour images et sequence
+$urlImages = "../images/annonces/";
+$intNoSeq = 1;
+
+// Requetes SQL pour les donnes de chaque annonces
+$sql = 'SELECT * FROM annonces WHERE Etat=:Etat';
+$query = $mysql->cBD->prepare($sql);
+$query->bindValue(':Etat', "1", PDO::PARAM_STR);
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_BOTH);
 ?>
+<div class="annonces">
+  <?php
+  foreach ($result as $annonce) {
+    $noAnnonce = $annonce[0];
+    $noUtil = $annonce[1];
+    $dateParution = $annonce[2];
+    $dateParution = substr($dateParution, 0, 10);
+    $categorie = $annonce[3];
+    $descAbregee = $annonce[4];
+    $descComplete = $annonce[5];
+    $prix = $annonce[6] . "$";
+    if ($prix == "0.00$") $prix = "N/A";
+    $photo = $urlImages . $annonce[7];
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/airpods.PNG" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">AirPods usé beaucoup trop</h5>
-            <p class="card-text">C$60
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+    $sql = 'SELECT Prenom, Nom FROM utilisateurs WHERE NoUtilisateur=:ID';
+    $query = $mysql->cBD->prepare($sql);
+    $query->bindValue(':ID', $noUtil, PDO::PARAM_STR);
+    $query->execute();
+    $nomPrenom = $query->fetchAll(PDO::FETCH_BOTH);
+    foreach ($nomPrenom as $nomAuteur) {
+      $auteur = $nomAuteur[0] . " " . $nomAuteur[1];
+    }
+
+  ?>
+
+  <div class="annonce">
+    <div class="card">
+      <div class="annonces_content">
+        <div class="annonces_image" style="max-width: 144px">
+          <img src="<?= $photo ?>" alt="..." style="max-width: 144px; max-height: 144px">
         </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img1.png" class="card-img-top" alt="...">
+        <div class="annonce_textes">
           <div class="card-body">
-            <h5 class="card-title">Air Jordan noir</h5>
-            <p class="card-text">C$169
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title"><a href="#"><?= $descAbregee ?></a></h5>
+            <ul class="list_annonce">
+              <li class="card-text">Prix demandé : <?= $prix ?></li>
+              <li><a href="#" class=card-text><?= $auteur ?></a></li>
+              <li class="card-text">Date de parution : <?= $dateParution ?></li>
+              <li class="card-text">No de l'annonce : <?= $noAnnonce ?></li>
+              <li class="card-text">No Sequentiel : <?= $intNoSeq ?></li>
+              <li><a href="#" class="btn btn-primary">Voir les détails</a></li>
+            </ul>
+            <!-- <p class="card-text">Prix demandé : <?= $prix ?></p>
+              <a href="#" class=card-text><?= $auteur ?></a>
+              <p class="card-text">Date de parution : <?= $dateParution ?></p>
+              <p class="card-text">No de l'annonce : <?= $noAnnonce ?></p>
+              <p class="card-text">No Sequentiel : <?= $intNoSeq ?></p> -->
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img2.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">PlayStation 4 avec manette invisible</h5>
-            <p class="card-text">C$460
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+
         </div>
       </div>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img3.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Beats Air 2</h5>
-            <p class="card-text">C$122
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img4.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Samsung Galaxy S10 128GB Prism Blue</h5>
-            <p class="card-text">C$450
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img5.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Scooter 330HP</h5>
-            <p class="card-text">C$6050
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-sm">
-      <div class=annonce>
-        <div class="card" style="width: 18rem;">
-          <img src="../images/img6.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Voiturette tel que vue
-            </h5>
-            <p class="card-text">C$5
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php
+    $intNoSeq++;
+  }
+  ?>
 </div>
-
 </body>
 
 </html>
