5cc91daeb6bb7296a1194ef97131f3fda854c4d4
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2021-05-20 17:32:58 -0400

    modif courreil envyoer

--- public/EnvoieEmailMotDePasse.php
+++ public/EnvoieEmailMotDePasse.php
@@ -1,5 +1,4 @@
 <?php
-//require_once("../dbconfig.php");
 require_once("envoyerMail.php");
 require_once("classe-mysql.php");
 
@@ -7,7 +6,6 @@ $strEmail = isset($_POST["email"]) ? $_POST["email"] : null;
 if ($strEmail != null) {
     $strInfosSensibles = "../dbconfig.php";
     $mysql = new mysql($strInfosSensibles);
-    //$cBD = new PDO("mysql:host=localhost;dbname=$strNomBD", $strNomAdmin, $strMotPasseAdmin);
     $sql = "SELECT MotDePasse FROM utilisateurs WHERE LOWER(Courriel) = LOWER('$strEmail')";
     $query = $mysql->cBD->prepare($sql);
     $query->execute();
@@ -15,8 +13,7 @@ if ($strEmail != null) {
     if (count($result) == 0) {
         echo false;
     } else {
-        envoyerMail($strEmail, "Clicker pour changer votre mot de passe", "http://localhost/ProjetFinal_Web/public/changerOublierMotDePasse.php?id=" . $mysql->getNoUtilisateurs($strEmail));
-        //envoyerMail($strEmail, "Mot de passe oublier", $result[0]["MotDePasse"]);
+        envoyerMail($strEmail, "Demande de changement de mot de passe", "Suivre le lien pour changer votre mot de passe : " . "http://localhost/ProjetFinal_Web/public/changerOublierMotDePasse.php?id=" . $mysql->getNoUtilisateurs($strEmail));
         echo true;
     }
 } else {
